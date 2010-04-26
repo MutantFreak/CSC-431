b950d49d790406796c889e05f83b7888c7ec79a6
Taylor Arnicar <MutantFrk@gmail.com>
2010-04-25 21:09:46 -0700

    Added test for all of the tokens generated in Lexer.fsl. Added a missing token and fixed a bug. Added support for strings to Parser.fsy.

--- lexer/ParserTests.fs
+++ lexer/ParserTests.fs
@@ -7,7 +7,6 @@ open Lexer
 open Parser
 open AST
 
-
 // given a string, produce the list of tokens
 // use this to test the Lexer
 let lexString (str : string) =
@@ -26,8 +25,18 @@ let parseString (str : string) =
     Parser.start Lexer.parsetokens lexbuf
 
 [<Fact>]
-let lexerTest1 () =
-    (lexString ";37;;") |> should equal [SEMICOLONTok;INTTok 37;SEMICOLONTok;SEMICOLONTok]
+let lexerTests () =
+    (lexString "")                        |> should equal []
+    (lexString ";37;;")                   |> should equal [SEMICOLONTok;INTTok 37;SEMICOLONTok;SEMICOLONTok]
+    (lexString "a b 2.4")                 |> should equal [IDTok "a";IDTok "b";DOUBLETok 2.4]
+    (lexString "a.2.3 b")                 |> should equal [IDTok "a";DOTTok;DOUBLETok 2.3;IDTok "b"]
+    (lexString "true false new else if")  |> should equal [TRUETok;FALSETok;NEWTok;ELSETok;IFTok]
+    (lexString "while function var q")    |> should equal [WHILETok;FUNCTIONTok;VARTok;IDTok "q"]
+    (lexString "&& || + - *")             |> should equal [ANDTok;ORTok;PLUSTok;MINUSTok;TIMESTok]
+    (lexString "/ < > <= >=")             |> should equal [DIVTok;LTTok;GTTok;LEQTok;GEQTok]
+    (lexString "= == =")                  |> should equal [EQTok;DOUBLEEQTok;EQTok]
+    (lexString "( ) { }")                 |> should equal [LPARENTok;RPARENTok;LCURLYTok;RCURLYTok]
+    (lexString "! . ,")                   |> should equal [BANGTok;DOTTok;COMMATok]
 
 [<Fact>]
 let t1 () =
