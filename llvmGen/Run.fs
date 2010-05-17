688c18581b3a566d27647f80bbd367e3711e6d85
Ryuho Kudo <ryuhokudo@gmail.com>
2010-05-17 16:09:56 -0700

    need this

---
+++ llvmGen/Run.fs
@@ -0,0 +1,14 @@
+module Run
+
+open AST
+open AST2
+open TypeDef
+open Generator
+
+(* Testing function that generates the llvm instruction list, and prints it out. *)
+let testFunc =
+    let declareLine = Declare(I64, "@add_prim")
+    let defineLine = Define(I64, "ourFunc", [])
+    let inputAST = (IntExp 4)
+    let (resultList, resultRegister) = generate inputAST (declareLine::[defineLine])
+    printLLVM (declareLine::(defineLine::resultList))
